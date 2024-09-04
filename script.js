de663e9ad5f97b718b0e5ecbee24fa61c0e94ccb
João Pedro Lopes <joaoplopes1604@gmail.com>
2024-09-04 12:17:45 -0300

    deixando o site responsivo e adicionando script

---
+++ script.js
@@ -0,0 +1,32 @@
+class MobileNavBar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active"
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass)
+        this.mobileMenu.classList.toggle(this.activeClass);
+    }
+    
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+    init() {
+        if(this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavBar = new MobileNavBar(
+    ".mobile_menu",
+    ".nav_list",
+    ".nav_list li",
+);
+mobileNavBar.init();
